fc78060742c6ad25089004567c9a9890f8736271
kcqg <wohlstandmarketing@gmail.com>
2018-01-18 00:06:04 +0800

    Add files via upload

--- progex/progex2-3.c
+++ progex/progex2-3.c
@@ -2,11 +2,11 @@
 
 int main(void){
 	
-	printf("Part No.	Price\n");
-	printf("T1267		$6.34\n");
-	printf("T1300		$8.92\n");
-	printf("T2401		$65.40\n");
-	printf("T4482		$36.99");
+	printf("Part No. Price\n");
+	printf("T1267	 $6.34\n");
+	printf("T1300	 $8.92\n");
+	printf("T2401	 $65.40\n");
+	printf("T4482	 $36.99");
 	
 	return 0;
 }
