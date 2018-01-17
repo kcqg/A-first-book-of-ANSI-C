ba9ab8ce9cd1b7ae14d1385ceb7111de82438d16
kcqg <wohlstandmarketing@gmail.com>
2018-01-17 23:08:35 +0800

    Add files via upload

---
+++ progsam/program2-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float radius = 2.0; 
+	float circumference = 2.0 * 3.1416 * radius;			
+	
+	printf("The circumference of the circle is %f\n", circumference);
+	
+	return 0;
+}
